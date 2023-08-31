704cdbfa247174be54ae39822181f96c4e8c4ee5
Tom Lee <thomas.j.lee@gmail.com>
2023-08-31 16:49:14 +0100

    use new url, make pipe-able

--- get_csv_local.sh
+++ get_csv_local.sh
@@ -10,5 +10,5 @@ tail -n+2 /tmp/xmas.csv | sort >> /tmp/xmas.csv.tmp
 mv /tmp/xmas.csv.tmp /tmp/xmas.csv
 gzip -f /tmp/xmas.csv
 
-$(dirname $0)/../.local/bin/aws s3 cp /tmp/xmas.csv.gz s3://sbma44/50q/sensors/environment/xmas-tree.csv --profile sensors --acl=public-read --cache-control  no-cache --content-encoding gzip > /dev/null
+$(dirname $0)/../.local/bin/aws s3 cp /tmp/xmas.csv.gz s3://sbma44/137t/sensors/environment/xmas-tree.csv --profile sensors --acl=public-read --cache-control  no-cache --content-encoding gzip
 rm -f /tmp/xmas.csv
